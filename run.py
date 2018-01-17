fe5016528bc00c53eabe8798c8deaf82be91fd79
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2018-01-17 14:18:16 -0600

    added session support

--- run.py
+++ run.py
@@ -22,8 +22,8 @@ def get_dep_ids(id_sources):
     return dep_ids
 
 
-def prepare_and_save_subtask(tool_class, app_name, filename, invocation_dict, participant_label, analysis_level,
-                             dep_ids):
+def prepare_and_save_subtask(tool_class, app_name, filename, invocation_dict, participant_label,
+                             analysis_level, dep_ids, session_label=None):
     subtask_dict = {
         "tool-class": tool_class,
         "description": "A {} BIDS app submission".format(app_name),
@@ -32,6 +32,8 @@ def prepare_and_save_subtask(tool_class, app_name, filename, invocation_dict, pa
         "required-to-post-process": True
     }
     subtask_dict['parameters']['participant_label'] = participant_label
+    if session_label:
+        subtask_dict['parameters']['session_label'] = session_label
     subtask_dict['parameters']['analysis_level'] = analysis_level
     if dep_ids:
         subtask_dict['prerequisites'] = dep_ids
@@ -49,9 +51,13 @@ if __name__ == '__main__':
     descriptor_dict = json.load(open(args.app_descriptor_file))
 
     levels = None
+    session_support = False
     for input in descriptor_dict['inputs']:
         if input['id'] == 'analysis_level':
             levels = input['value-choices']
+        elif input['id'] == 'session_label':
+            session_support = True
+
     assert levels, "analysis_level must have value-choices"
 
     invocation_dict = json.load(open(args.invocation_file))
@@ -66,6 +72,15 @@ if __name__ == '__main__':
     else:
         participants_to_analyze = layout.get_subjects()
 
+    if 'session_label' in invocation_dict.keys():
+        sessions_to_analyze = invocation_dict['session_label']
+    # for all sessions
+    else:
+        sessions_to_analyze = layout.get_sessions()
+
+    if not session_support:
+        sessions_to_analyze = None
+
     print(levels)
     print(participants_to_analyze)
 
@@ -73,7 +88,23 @@ if __name__ == '__main__':
     for level in levels:
         id_sources = []
 
-        if level.startswith('participant'):
+        if level.startswith('session') and session_support:
+            for participant in participants_to_analyze:
+                for session in sessions_to_analyze:
+                    filename = "subtask_%s_%s_%s.json" % (level, participant, session)
+                    prepare_and_save_subtask(tool_class=args.app_descriptor_file,
+                                             app_name=descriptor_dict['name'],
+                                             filename=filename,
+                                             invocation_dict=invocation_dict,
+                                             participant_label=participant,
+                                             session_label=session,
+                                             analysis_level=level,
+                                             dep_ids=dep_ids)
+                    id_sources.append(filename.replace('.json', '.*bid'))
+
+            dep_ids = get_dep_ids(id_sources)
+
+        elif level.startswith('participant'):
             for participant in participants_to_analyze:
                 filename = "subtask_%s_%s.json" % (level, participant)
                 prepare_and_save_subtask(tool_class=args.app_descriptor_file,
@@ -81,6 +112,7 @@ if __name__ == '__main__':
                                          filename=filename,
                                          invocation_dict=invocation_dict,
                                          participant_label=participant,
+                                         session_label=sessions_to_analyze,
                                          analysis_level=level,
                                          dep_ids=dep_ids)
                 id_sources.append(filename.replace('.json', '.*bid'))
@@ -94,6 +126,7 @@ if __name__ == '__main__':
                                      filename=filename,
                                      invocation_dict=invocation_dict,
                                      participant_label=participants_to_analyze,
+                                     session_label=sessions_to_analyze,
                                      analysis_level=level,
                                      dep_ids=dep_ids)
             id_sources.append(filename.replace('.json', '.*bid'))
